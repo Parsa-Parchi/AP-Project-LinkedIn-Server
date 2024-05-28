88950ac069c24f3d74984446eae2275c7b986af6
Parsa-Parchi <parsaparachi828282@gmail.com>
2024-05-28 19:23:34 +0330

    feat : add User Class with fields

---
+++ src/test/java/example/server/models/User.java
@@ -0,0 +1,19 @@
+package example.server.models;
+
+public class User {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private String additionalName;
+    private String avatar_url;
+    private String background_url;
+    private String headline;
+    private String country;
+    private String city;
+    private int followers;
+    private int following;
+    private int connections;
+
+}
