d1754b0bc2d3806e6c5d69fd9fb7c276dc7b072d
Parsa-Parchi <parsaparachi828282@gmail.com>
2024-05-28 19:35:09 +0330

    feat : add Skill Class with fields.

---
+++ src/test/java/example/server/models/Skill.java
@@ -0,0 +1,13 @@
+package example.server.models;
+
+public class Skill {
+
+    private String Skill_1;
+    private String skill_2;
+    private String skill_3;
+    private String skill_4;
+    private String skill_5;
+    private int id;
+    private String email;
+}
+
