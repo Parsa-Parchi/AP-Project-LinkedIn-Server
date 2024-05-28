af31387eeaeb2a845c6fe680fdcfdd794a25020f
Parsa-Parchi <parsaparachi828282@gmail.com>
2024-05-28 20:14:54 +0330

    feat : add constructor and getter , setter methods .

--- src/test/java/example/server/models/Comment.java
+++ src/test/java/example/server/models/Comment.java
@@ -6,4 +6,59 @@ public class Comment {
     private String email;
     private String commentDate;
     private String comment;
+
+    public Comment(int id, int postId, String email, String commentDate, String comment) {
+        this.id = id;
+        this.postId = postId;
+        this.email = email;
+        this.commentDate = commentDate;
+        this.comment = comment;
+    }
+
+    public Comment(int postId, String email, String comment) {
+        this.postId = postId;
+        this.email = email;
+        this.comment = comment;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getPostId() {
+        return postId;
+    }
+
+    public void setPostId(int postId) {
+        this.postId = postId;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCommentDate() {
+        return commentDate;
+    }
+
+    public void setCommentDate(String commentDate) {
+        this.commentDate = commentDate;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
 }
+
