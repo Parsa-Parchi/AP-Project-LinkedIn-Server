77f71e479e46a98859e2aad7d12a8dabf7454b02
Parsa-Parchi <parsaparachi828282@gmail.com>
2024-05-28 19:50:37 +0330

    feat : add Post Class with fields.

---
+++ src/test/java/example/server/models/Post.java
@@ -0,0 +1,11 @@
+package example.server.models;
+
+public class Post {
+    private int id;
+    private int likes;
+    private int comments;
+    private String author;
+    private String title;
+    private String content;
+    private String createdAt;
+}
