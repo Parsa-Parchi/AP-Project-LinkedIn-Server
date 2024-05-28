c71f74b842a9b6b05048d6666636e5ab8961699c
Parsa-Parchi <parsaparachi828282@gmail.com>
2024-05-28 20:12:38 +0330

    feat : add Comment Class with its fields.

---
+++ src/test/java/example/server/models/Comment.java
@@ -0,0 +1,9 @@
+package example.server.models;
+
+public class Comment {
+    private int id;
+    private int postId;
+    private String email;
+    private String commentDate;
+    private String comment;
+}
