b8a9a4f76f41836d3a028035f648bc2067943520
Parsa-Parchi <parsaparachi828282@gmail.com>
2024-05-29 18:29:02 +0330

    feat : add insertSkill and updateSkill methods of Skill_DataBase Class.

--- src/test/java/example/server/DataBase/Skill_DataBase.java
+++ src/test/java/example/server/DataBase/Skill_DataBase.java
@@ -1,5 +1,7 @@
 package example.server.DataBase;
 
+import example.server.models.Skill;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
@@ -24,5 +26,29 @@ public class Skill_DataBase {
                 + "FOREIGN KEY (email) REFERENCES users(email) ON DELETE CASCADE"
                 + ");");
         statement.executeUpdate();
+    }
+
+    public void insertSkill(Skill skill) throws SQLException {
+        PreparedStatement statement = connection.prepareStatement("INSERT INTO skills (email, skill_1, skill_2, skill_3, skill_4, skill_5) VALUES (?, ?, ?, ?, ?, ?)");
+        statement.setString(1, skill.getEmail());
+        statement.setString(2, skill.getSkill_1());
+        statement.setString(3, skill.getSkill_2());
+        statement.setString(4, skill.getSkill_3());
+        statement.setString(5, skill.getSkill_4());
+        statement.setString(6, skill.getSkill_5());
+        statement.executeUpdate();
+
+    }
+
+    public void updateSkill(Skill skill) throws SQLException {
+        PreparedStatement statement = connection.prepareStatement("UPDATE skills SET skill_1 = ?, skill_2 = ?, skill_3 = ?, skill_4 = ?, skill_5 = ? WHERE email = ?");
+        statement.setString(1, skill.getSkill_1());
+        statement.setString(2, skill.getSkill_2());
+        statement.setString(3, skill.getSkill_3());
+        statement.setString(4, skill.getSkill_4());
+        statement.setString(5, skill.getSkill_5());
+        statement.setString(6, skill.getEmail());
+        statement.executeUpdate();
+
     }
 }
