330cd15fb7c4b8c9bf2d5d6404616b835f554d82
Parsa-Parchi <parsaparachi828282@gmail.com>
2024-05-29 16:13:06 +0330

    feat : add getUser and getAllUsers methods of User_DataBase Class for retrieving user's data.

--- src/test/java/example/server/DataBase/User_DataBase.java
+++ src/test/java/example/server/DataBase/User_DataBase.java
@@ -79,4 +79,66 @@ public class User_DataBase {
         PreparedStatement statement = connection.prepareStatement("DELETE FROM users");
         statement.executeUpdate();
     }
+
+    public User getUsers(String email) throws SQLException {
+        PreparedStatement statement = connection.prepareStatement("SELECT * FROM users WHERE email = ?");
+        statement.setString(1, email);
+        ResultSet resultSet = statement.executeQuery();
+
+        if (resultSet.next()) {
+            int id = resultSet.getInt("id");
+            String userEmail = resultSet.getString("email");
+            String password = resultSet.getString("password");
+            String firstName = resultSet.getString("name");
+            String lastName = resultSet.getString("lastName");
+            String additionalName = resultSet.getString("additionalName");
+            String avatarUrl = resultSet.getString("avatar_url");
+            String backgroundUrl = resultSet.getString("background_url");
+            String headline = resultSet.getString("headline");
+            String country = resultSet.getString("country");
+            String city = resultSet.getString("city");
+            int followers = resultSet.getInt("followers");
+            int followings = resultSet.getInt("followings");
+            int connections = resultSet.getInt("connections");
+
+            return new User(id, userEmail, password, firstName, lastName, additionalName, avatarUrl,
+                    backgroundUrl, headline, country, city, followers, followings, connections);
+        }
+
+        return null;
+
+    }
+
+    public ArrayList<User> getAllUsers() throws SQLException {
+        PreparedStatement statement = connection.prepareStatement("SELECT * FROM users");
+        ResultSet resultSet = statement.executeQuery();
+        ArrayList<User> users = new ArrayList<>();
+
+        while (resultSet.next()) {
+            int id = resultSet.getInt("id");
+            String userEmail = resultSet.getString("email");
+            String password = resultSet.getString("password");
+            String firstName = resultSet.getString("name");
+            String lastName = resultSet.getString("lastName");
+            String additionalName = resultSet.getString("additionalName");
+            String avatarUrl = resultSet.getString("avatar_url");
+            String backgroundUrl = resultSet.getString("background_url");
+            String headline = resultSet.getString("headline");
+            String country = resultSet.getString("country");
+            String city = resultSet.getString("city");
+            int followers = resultSet.getInt("followers");
+            int followings = resultSet.getInt("followings");
+            int connections = resultSet.getInt("connections");
+
+            User user = new User(id, userEmail, password, firstName, lastName, additionalName,
+                    avatarUrl, backgroundUrl, headline, country, city, followers, followings, connections);
+
+            users.add(user);
+        }
+
+        return users;
+
+    }
+
+
 }
