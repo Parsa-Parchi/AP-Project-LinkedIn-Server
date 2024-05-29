16d6680a5409ff27391171ca8a956562039c4989
Parsa-Parchi <parsaparachi828282@gmail.com>
2024-05-29 12:06:47 +0330

    feat : add ContactInformation Class with fields.

---
+++ src/test/java/example/server/models/ContactInformation.java
@@ -0,0 +1,16 @@
+package example.server.models;
+
+import java.util.Date;
+
+public class ContactInformation {
+    private int id;
+    private String email;
+    private String viewLink;
+    private String Mobile_PhoneNumber;
+    private String Home_PhoneNumber;
+    private String Workplace_PhoneNumber;
+    private String address;
+    private Date birthDate;
+    private String fastConnect;
+    private String visibility;
+}
