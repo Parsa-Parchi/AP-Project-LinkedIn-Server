4f478f87e3f8d2d2d4a5d39a7ac5b6008ec46455
Parsa-Parchi <parsaparachi828282@gmail.com>
2024-05-29 23:16:51 +0330

    feat : add getter method for Hashtag_DataBase Class.

--- src/test/java/example/server/DataBase/Hashtag_DataBase.java
+++ src/test/java/example/server/DataBase/Hashtag_DataBase.java
@@ -4,7 +4,9 @@ import example.server.models.Hashtag;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class Hashtag_DataBase {
     private final Connection connection;
@@ -67,6 +69,37 @@ public class Hashtag_DataBase {
         preparedStatement.executeUpdate();
 
     }
+
+    public void deleteAllHashtags() throws SQLException {
+        PreparedStatement preparedStatement = connection.prepareStatement("DELETE FROM hashtags");
+        preparedStatement.executeUpdate();
+
+    }
+
+    public Hashtag getHashtag(int id) throws SQLException {
+        PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM hashtags WHERE id = ?");
+        preparedStatement.setInt(1, id);
+        ResultSet resultSet = preparedStatement.executeQuery();
+
+        if (resultSet.next()) {
+            return new Hashtag(resultSet.getInt("id"), resultSet.getString("hashtag"));
+
+        }
+        return null;
+    }
+
+    public ArrayList<String> getHashtagsOfPost(int postId) throws SQLException {
+        PreparedStatement preparedStatement = connection.prepareStatement("SELECT hashtag FROM hashtags WHERE post_id = ?");
+        preparedStatement.setInt(1, postId);
+        ResultSet resultSet = preparedStatement.executeQuery();
+        ArrayList<String> hashtags = new ArrayList<>();
+        while (resultSet.next()) {
+            hashtags.add(resultSet.getString("hashtag"));
+
+        }
+        return hashtags;
+    }
+
 
 
 }
