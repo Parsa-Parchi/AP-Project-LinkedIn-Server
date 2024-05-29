0d82dd8538838b2ec26e39ba5385cc16e3e78f63
Parsa-Parchi <parsaparachi828282@gmail.com>
2024-05-29 12:13:18 +0330

    feat : add Profile Class with fields .

---
+++ src/test/java/example/server/models/Profile.java
@@ -0,0 +1,12 @@
+package example.server.models;
+
+import java.util.ArrayList;
+
+public class Profile {
+    private User user;
+    private Skill skill;
+    private ContactInformation contactInformation;
+    private ArrayList<Education> educations;
+
+
+}
