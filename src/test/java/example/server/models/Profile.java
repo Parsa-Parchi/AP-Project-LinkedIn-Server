04cd78c66641aef0914fcefbad3b370c1ef1cc7d
Parsa-Parchi <parsaparachi828282@gmail.com>
2024-05-29 12:15:42 +0330

    feat : add constructor and getter , setter methods of Profile Class.

--- src/test/java/example/server/models/Profile.java
+++ src/test/java/example/server/models/Profile.java
@@ -9,4 +9,46 @@ public class Profile {
     private ArrayList<Education> educations;
 
 
+    public Profile(User user, Skill skill, ContactInformation contactInformation, ArrayList<Education> educations) {
+        this.user = user;
+        this.skill = skill;
+        this.contactInformation = contactInformation;
+        this.educations = educations;
+    }
+
+    public Profile(User user) {
+        this.user = user;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Skill getSkill() {
+        return skill;
+    }
+
+    public void setSkill(Skill skill) {
+        this.skill = skill;
+    }
+
+    public ContactInformation getContactInformation() {
+        return contactInformation;
+    }
+
+    public void setContactInformation(ContactInformation contactInformation) {
+        this.contactInformation = contactInformation;
+    }
+
+    public ArrayList<Education> getEducations() {
+        return educations;
+    }
+
+    public void setEducations(ArrayList<Education> educations) {
+        this.educations = educations;
+    }
 }
