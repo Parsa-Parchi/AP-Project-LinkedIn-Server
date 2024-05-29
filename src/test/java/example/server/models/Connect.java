ed25d9e1b1a7f9c9e852bfd2be798291f4a2dc5c
Parsa-Parchi <parsaparachi828282@gmail.com>
2024-05-29 11:47:42 +0330

    feat : add Connect Class with fields.

---
+++ src/test/java/example/server/models/Connect.java
@@ -0,0 +1,8 @@
+package example.server.models;
+
+public class Connect {
+    private int id;
+    private String Request_Sender;
+    private String Request_Receiver;
+    private boolean accepted;
+}
