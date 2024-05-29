add5611159992f429c0624be0ad9a1cace956ee1
Parsa-Parchi <parsaparachi828282@gmail.com>
2024-05-29 23:03:32 +0330

    feat : add insert , update and delete methods for Hashtag.

--- src/test/java/example/server/DataBase/Hashtag_DataBase.java
+++ src/test/java/example/server/DataBase/Hashtag_DataBase.java
@@ -1,5 +1,7 @@
 package example.server.DataBase;
 
+import example.server.models.Hashtag;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
@@ -21,5 +23,50 @@ public class Hashtag_DataBase {
 
         preparedStatement.executeUpdate();
     }
+
+    public void insertHashtag(Hashtag hashtag) throws SQLException {
+        PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO hashtags (post_id, hashtag) VALUES (?, ?)");
+        preparedStatement.setInt(1, hashtag.getPostId());
+        preparedStatement.setString(2, hashtag.getHashtag());
+        preparedStatement.executeUpdate();
+
+    }
+
+    public void UpdateHashtag(Hashtag hashtag) throws SQLException {
+        PreparedStatement preparedStatement = connection.prepareStatement("UPDATE hashtags SET hashtag = ? WHERE id = ?");
+        preparedStatement.setString(1, hashtag.getHashtag());
+        preparedStatement.setInt(2, hashtag.getId());
+        preparedStatement.executeUpdate();
+
+    }
+
+    public void deleteHashtag(Hashtag hashtag) throws SQLException {
+        PreparedStatement preparedStatement = connection.prepareStatement("DELETE FROM hashtags WHERE id = ?");
+        preparedStatement.setInt(1, hashtag.getId());
+        preparedStatement.executeUpdate();
+
+    }
+
+    public void deleteHashtag(int id) throws SQLException {
+        PreparedStatement preparedStatement = connection.prepareStatement("DELETE FROM hashtags WHERE id = ?");
+        preparedStatement.setInt(1, id);
+        preparedStatement.executeUpdate();
+
+    }
+
+    public void deleteHashtag(String hashtag) throws SQLException {
+        PreparedStatement preparedStatement = connection.prepareStatement("DELETE FROM hashtags WHERE hashtag = ?");
+        preparedStatement.setString(1, hashtag);
+        preparedStatement.executeUpdate();
+
+    }
+
+    public void deleteHashtagsOfPost(int postId) throws SQLException {
+        PreparedStatement preparedStatement = connection.prepareStatement("DELETE FROM hashtags WHERE post_id = ?");
+        preparedStatement.setInt(1, postId);
+        preparedStatement.executeUpdate();
+
+    }
+
 
 }
