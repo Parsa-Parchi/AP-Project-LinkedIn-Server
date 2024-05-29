39963076d879d010c4227c65f62a2fb63978605a
Parsa-Parchi <parsaparachi828282@gmail.com>
2024-05-29 11:50:09 +0330

    feat : add constructor and getter,setter methods of Connect class.

--- src/test/java/example/server/models/Connect.java
+++ src/test/java/example/server/models/Connect.java
@@ -5,4 +5,48 @@ public class Connect {
     private String Request_Sender;
     private String Request_Receiver;
     private boolean accepted;
+
+    public Connect(int id, String request_Sender, String request_Receiver, boolean accepted) {
+        this.id = id;
+        Request_Sender = request_Sender;
+        Request_Receiver = request_Receiver;
+        this.accepted = accepted;
+    }
+
+    public Connect(String request_Sender, String request_Receiver) {
+        Request_Sender = request_Sender;
+        Request_Receiver = request_Receiver;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getRequest_Sender() {
+        return Request_Sender;
+    }
+
+    public void setRequest_Sender(String request_Sender) {
+        Request_Sender = request_Sender;
+    }
+
+    public String getRequest_Receiver() {
+        return Request_Receiver;
+    }
+
+    public void setRequest_Receiver(String request_Receiver) {
+        Request_Receiver = request_Receiver;
+    }
+
+    public boolean isAccepted() {
+        return accepted;
+    }
+
+    public void setAccepted(boolean accepted) {
+        this.accepted = accepted;
+    }
 }
