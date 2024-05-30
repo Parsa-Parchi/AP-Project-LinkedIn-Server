7dd3edcea73e8e4af9230500e74cf2d19d09fee4
Parsa-Parchi <parsaparachi828282@gmail.com>
2024-05-30 11:21:00 +0330

    feat : add insert and delete methods of Follow_DataBase for saving or deleting data from database.

--- src/test/java/example/server/DataBase/Follow_DataBase.java
+++ src/test/java/example/server/DataBase/Follow_DataBase.java
@@ -3,6 +3,7 @@ package example.server.DataBase;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import example.server.models.Follow;
 
 public class Follow_DataBase {
     private final Connection connection;
@@ -24,6 +25,46 @@ public class Follow_DataBase {
         statement.executeUpdate();
 
     }
+
+
+    public void insertFollow(Follow follow) throws SQLException{
+
+        PreparedStatement statement =connection.prepareStatement("INSERT INTO follows(follower,followed) VALUES(?,?)");
+        statement.setString(1, follow.getFollower());
+        statement.setString(2, follow.getFollowed());
+        statement.executeUpdate();
+
+    }
+
+    public void deleteFollow(Follow follow) throws SQLException{
+        PreparedStatement statement = connection.prepareStatement("DELETE FROM follows WHERE follower = ? AND followed = ?");
+        statement.setString(1, follow.getFollower());
+        statement.setString(2, follow.getFollowed());
+        statement.executeUpdate();
+
+    }
+
+    public void deleteFollow(int id) throws SQLException{
+        PreparedStatement statement = connection.prepareStatement("DELETE FROM follows WHERE id = ?");
+        statement.setInt(1, id);
+        statement.executeUpdate();
+
+    }
+
+    public void deleteFollowersOfUser(String followedEmail) throws SQLException{
+        PreparedStatement statement = connection.prepareStatement("DELETE FROM follows WHERE followed = ?");
+        statement.setString(1, followedEmail);
+        statement.executeUpdate();
+
+    }
+
+    public void deleteAllFollows() throws SQLException{
+        PreparedStatement statement = connection.prepareStatement("DELETE FROM follows");
+        statement.executeUpdate();
+
+    }
+
+
 
 
 }
