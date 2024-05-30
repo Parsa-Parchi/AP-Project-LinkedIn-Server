7ef2b1d05727b283037b8cad68ae4d228829177f
Parsa-Parchi <parsaparachi828282@gmail.com>
2024-05-30 11:43:58 +0330

    feat : add insert , update and delete methods of Education_DataBase Class .

--- src/test/java/example/server/DataBase/Education_DataBase.java
+++ src/test/java/example/server/DataBase/Education_DataBase.java
@@ -1,6 +1,9 @@
 package example.server.DataBase;
 
+import example.server.models.Education;
+
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
@@ -28,6 +31,59 @@ public class Education_DataBase {
 
         statement.executeUpdate();
     }
+
+    public void insertEducation(Education education) throws SQLException {
+        PreparedStatement statement = connection.prepareStatement("INSERT INTO education (email, school_name, field, grade, start_date, end_date, Activity_Community, description) "
+                + "VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
+
+        statement.setString(1, education.getEmail());
+        statement.setString(2, education.getSchoolName());
+        statement.setString(3, education.getField());
+        statement.setDouble(4, education.getGrade());
+        statement.setDate(5, (Date) education.getStartDate());
+        statement.setDate(6, (Date) education.getEndDate());
+        statement.setString(7,education.getActivity_Community());
+        statement.setString(8,education.getDescription());
+        statement.executeUpdate();
+
+    }
+
+    public void updateEducation(Education education) throws SQLException {
+        PreparedStatement statement = connection.prepareStatement("UPDATE education "
+                + "SET school_name = ?, field = ?, grade = ?, start_date = ?, end_date = ?, Activity_Community = ?, description = ?"
+                + "WHERE email = ?");
+
+        statement.setString(1, education.getSchoolName());
+        statement.setString(2, education.getField());
+        statement.setDouble(3, education.getGrade());
+        statement.setDate(4, (Date) education.getStartDate());
+        statement.setDate(5, (Date) education.getEndDate());
+        statement.setString(6, education.getActivity_Community());
+        statement.setString(7, education.getDescription());
+        statement.setString(8, education.getEmail());
+        statement.executeUpdate();
+
+    }
+
+    public void deleteEducation(int id) throws SQLException {
+        PreparedStatement statement = connection.prepareStatement("DELETE FROM education WHERE id = ?");
+        statement.setInt(1, id);
+        statement.executeUpdate();
+
+    }
+
+    public void deleteEducationOfUser(String email) throws SQLException {
+        PreparedStatement statement = connection.prepareStatement("DELETE FROM education WHERE email = ?");
+        statement.setString(1, email);
+        statement.executeUpdate();
+    }
+
+    public void deleteAllEducations() throws SQLException {
+        PreparedStatement statement = connection.prepareStatement("DELETE FROM education");
+        statement.executeUpdate();
+
+    }
+
 
 
 
