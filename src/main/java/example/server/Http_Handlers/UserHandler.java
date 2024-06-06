f0cc759aaacd0fbbc18124530964706864e4ac00
Parsa-Parchi <parsaparachi828282@gmail.com>
2024-06-06 20:53:32 +0330

    feat : add getContactInfoOfUserHandler method in UserHandler for managing HTTP request of get contactInfo of User.

--- src/main/java/example/server/Http_Handlers/UserHandler.java
+++ src/main/java/example/server/Http_Handlers/UserHandler.java
@@ -1,10 +1,12 @@
 package example.server.Http_Handlers;
 
+import example.server.Controller.ContactInfo_Controller;
 import example.server.Controller.Skill_Controller;
 import example.server.Controller.UserController;
 import example.server.Server;
 import example.server.Utilities.Authorization_Util;
 import example.server.Utilities.jwt_Util;
+import example.server.models.ContactInformation;
 import example.server.models.Skill;
 import example.server.models.User;
 
@@ -129,4 +131,31 @@ public class UserHandler {
         }
     }
 
+    public static void getContactInfoOfUserHandler(HttpExchange exchange) throws IOException {
+        String token = Authorization_Util.getAuthToken(exchange);
+        if (token == null) {
+            Server.sendResponse(exchange, 401, gson.toJson(Collections.singletonMap("error ", "Authorization token is missing or invalid")));
+            return;
+        }
+
+        else if(!Authorization_Util.validateAuthToken(exchange,token)) {
+            Server.sendResponse(exchange, 401, gson.toJson(Collections.singletonMap("error ", "Invalid or expired token")));
+            return;
+        }
+        String EmailOfToken = jwt_Util.parseToken(token);
+
+        try {
+            ContactInformation contactInformation = ContactInfo_Controller.getContactInfoOfUser(EmailOfToken);
+            if(contactInformation == null)
+                Server.sendResponse(exchange, 404, gson.toJson(Collections.singletonMap("error", "ContactInformation not found")));
+            else
+                Server.sendResponse(exchange, 200, gson.toJson(contactInformation));
+        }
+        catch (SQLException e){
+            Server.sendResponse(exchange, 500, "A problem was found in the Database : " + e.getMessage());
+        }
+        catch (Exception e){
+            Server.sendResponse(exchange, 500, "Internal Server error: " + e.getMessage());
+        }
+    }
 }
--- src/main/java/example/server/Router/Router.java
+++ src/main/java/example/server/Router/Router.java
@@ -13,6 +13,7 @@ public class Router {
         server.get("/user",UserHandler::retrieveUserHandler);
         server.get("/users",UserHandler::retrieveAllUserHandler);
         server.get("/user/skill",UserHandler::getSkillOfUserHandler);
+        server.get("/user/contact-info",UserHandler::getSkillOfUserHandler);
 
     }
 
