bea914c9db4a99eee39c78e50dcedb1bf4a6c345
Parsa-Parchi <parsaparachi828282@gmail.com>
2024-06-28 14:58:14 +0330

    feat : add Media Class for handling media transmitting between client and server .

---
+++ src/main/java/example/server/models/Media.java
@@ -0,0 +1,80 @@
+package example.server.models;
+
+import java.sql.Timestamp;
+
+public class Media {
+    private int id;
+    private int postId;
+    private String filePath;
+    private String fileName;
+    private String fileType;
+    private long fileSize;
+    private Timestamp uploadDate;
+
+    public Media(int id, int postId, String filePath, String fileName, String fileType, long fileSize, Timestamp uploadDate) {
+        this.id = id;
+        this.postId = postId;
+        this.filePath = filePath;
+        this.fileName = fileName;
+        this.fileType = fileType;
+        this.fileSize = fileSize;
+        this.uploadDate = uploadDate;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getPostId() {
+        return postId;
+    }
+
+    public void setUserId(int userId) {
+        this.postId = postId;
+    }
+
+    public String getFilePath() {
+        return filePath;
+    }
+
+    public void setFilePath(String filePath) {
+        this.filePath = filePath;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public String getFileType() {
+        return fileType;
+    }
+
+    public void setFileType(String fileType) {
+        this.fileType = fileType;
+    }
+
+    public long getFileSize() {
+        return fileSize;
+    }
+
+    public void setFileSize(long fileSize) {
+        this.fileSize = fileSize;
+    }
+
+    public Timestamp getUploadDate() {
+        return uploadDate;
+    }
+
+    public void setUploadDate(Timestamp uploadDate) {
+        this.uploadDate = uploadDate;
+    }
+
+}
